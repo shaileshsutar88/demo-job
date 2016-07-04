530dac5a18f131a47a5e87bde8e08bcedf193636
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-07-04 09:43:53 +0530

    Region change added.

--- tf-test.tf
+++ tf-test.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "KIAIBUKRUL3JDDMQX6A"
   secret_key = "ze/8iw8m6W9fvJFyGfpIX2mrNy1XvAmrRMyk0y/j"
-  region     = "ap-south-1"
+  region     = "ap-southeast-1"
 }
 
 resource "aws_instance" "tf-test" {
