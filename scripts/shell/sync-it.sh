c5c144c7da96c2956c2c08fb091b1540d5a64985
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-09-07 16:19:11 +0530

    updates in shell scripts

--- scripts/shell/sync-it.sh
+++ scripts/shell/sync-it.sh
@@ -6,8 +6,9 @@
 # Purpose : Shell script to sync Documentroot changes with master server.
 #
 user=`whoami`
+user1='ec2-user'
 key="/home/$user/scripts/key"
-src='ec2-user@172.31.x.x:/var/www/html/'
+src="$user1@172.31.x.x:/var/www/html/"
 dst="/var/www/html/"
 sudo mv /var/www/html /var/www/html_`date +%d%m%Y`
 sudo mkdir /var/www/html
