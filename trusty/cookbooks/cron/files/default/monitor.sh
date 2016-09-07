f23a5f89be606d020c1ca794bb163229f1a85267
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-09-07 14:37:00 +0530

    health check script update

--- trusty/cookbooks/cron/files/default/monitor.sh
+++ trusty/cookbooks/cron/files/default/monitor.sh
@@ -5,17 +5,12 @@
 #
 #
 sudo mkdir /home/vagrant/scripts
-#cat <<EOT>> /home/vagrant/scripts/health-chech.sh
-TO_ADDRESS="root@test"
+ip="10.0.0.2"
+TO_ADDRESS="root@$ip"
 SUBJECT="Nginx serive is down"
 BODY="This is a linux mail system. Nginx service is down nginx-web server.."
-result=`curl -s 10.0.0.2:80 >/dev/null && echo Connected. || echo Fail.`
+result=`curl -s $ip:80 >/dev/null && echo Connected. || echo Fail.`
 if [ $result == "Fail" ]
 then
 	echo ${BODY}| mail -s ${SUBJECT} ${TO_ADDRESS}
 fi
-#EOT
-#sudo chmod +x /home/vagrant/scripts/health-check.sh
-#l0="*/5 * * * * /home/vagrant/scripts/health-check.sh"
-#sudo (crontab -u $vagrant -l; echo `"$l0"` ) | crontab -u vagrant -
-
