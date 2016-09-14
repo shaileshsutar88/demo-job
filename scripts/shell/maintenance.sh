5e9f3c64c7011ddb7d4936496ba17974b3a02a51
Ketan Muneshwar <ketan.muneshwar@yourkarma.com>
2016-09-15 02:44:59 +0530

    Maintenace script - In progress

--- scripts/shell/maintenance.sh
+++ scripts/shell/maintenance.sh
@@ -1,60 +1,46 @@
 #!/bin/bash
 #
-#
-# Purpose : Shell script to grant/revoke access to user for particular group of servers.
+# Purpose : Shell script to deploy Maintenance page before actual web application deployment - Ubuntu.
 #
 # Author : Shailesh Sutar
 #
-# Date : 28th July 2016
+# Date : 15th September 2016
 # 
+# Environment : Ubuntu 16.04 LTS with nginx and php-fpm
+#
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage:"
-    echo "./dev-enable.sh <username> <file> <public-key-file> <access-type>"
+    echo "./maintenace-ubuntu.sh <deployment-state>"
     echo "username should name of users who's access needs to be enable or disable "
     echo "file which contains list of IP's of servers"
-    echo "acess type should either 'grant' or 'revoke' particularly"
+    echo "state type should either 'production' or 'maintenance' "
     exit 1
 fi
-user=$1
-file=$2
-keyfile=$3
-access=$4
-basekey="/home/test-user/aws/test.pem" 
-userkey="/home/test-user/.ssh/id_rsa.pub"
-user1="ec2-user"
+state=$1
+serv1="/etc/init.d/nginx"
+serv2="/etc/init.d/php7.0-fpm"
 
-function grant-access {
-	for line in `cat $file`
-	do
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo useradd -m -d /home/$user $user
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo mkdir /home/$user/.ssh
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo touch /home/$user/.ssh/authorized_keys
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo chmod 777 -R /home/$user/.ssh
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo bash -c "'cat > /home/$user/.ssh/authorized_keys'" < $userkey
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo chmod 600 /home/$user/.ssh/authorized_keys
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo chown $user.$user /home/$user/.ssh/authorized_keys
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo chmod 700 /home/$user/.ssh
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo chown $user.$user /home/$user/.ssh
-	done
+function production {
+	sudo sed -i "s|/var/www/maintenace|/var/www/html|g" /etc/nginx/conf.d/production.conf
+	sudo sed -i "s|/var/www/html|/var/www/maintenance|g" /etc/nginx/conf.d/maintenance.conf
+	sudo $serv1 restart
+	sudo $serv2 restart
 }
 
-function revoke-access {
-	for line in `cat $file`
-	do
-		#echo ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo userdel -f $user
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo userdel -f $user
-		echo "Deleting users .ssh directory with key access"
-		ssh -i $basekey -o strictHostKeyChecking=no $user1@$line sudo rm -rf /home/$user/.ssh
-	done
+function maintenance {
+	sudo sed -i "s|/var/www/html|/var/www/maintenance|g" /etc/nginx/conf.d/production.conf
+	sudo sed -i "s|/var/www/maintenance|/var/www/html|g" /etc/nginx/conf.d/maintenance.conf
+        sudo $serv1 restart
+        sudo $serv2 restart
 }
 
-if [ $access == "grant" ]
+if [ $state == "maintenance" ]
 then
-	grant-access
-elif [ $access == "revoke" ]
+	maintenance
+elif [ $state == "production" ]
 then
-	revoke-access
+	production
 else
-	echo " Wrong Keyword!!!! Use only grant or revoke"
+	echo " Wrong Keyword!!!! Use only production or maintenance."
 fi
