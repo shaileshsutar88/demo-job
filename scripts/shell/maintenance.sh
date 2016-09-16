746ab090865962583e675a6347b53a5ff35c2eff
Shailesh Sutar <shaileshsutar88@gmail.com>
2016-09-16 16:48:04 +0530

    Added git-ignore

--- scripts/shell/maintenance.sh
+++ scripts/shell/maintenance.sh
@@ -22,15 +22,15 @@ serv1="/etc/init.d/nginx"
 serv2="/etc/init.d/php7.0-fpm"
 
 function production {
-	sudo sed -i "s|/var/www/maintenace|/var/www/html|g" /etc/nginx/conf.d/production.conf
-	sudo sed -i "s|/var/www/html|/var/www/maintenance|g" /etc/nginx/conf.d/maintenance.conf
+	sudo sed -i "s|/var/www/maintenace|/var/www/example|g" /etc/nginx/conf.d/production.conf
+	sudo sed -i "s|/var/www/example|/var/www/maintenance|g" /etc/nginx/conf.d/maintenance.conf
 	sudo $serv1 restart
 	sudo $serv2 restart
 }
 
 function maintenance {
-	sudo sed -i "s|/var/www/html|/var/www/maintenance|g" /etc/nginx/conf.d/production.conf
-	sudo sed -i "s|/var/www/maintenance|/var/www/html|g" /etc/nginx/conf.d/maintenance.conf
+	sudo sed -i "s|/var/www/example|/var/www/maintenance|g" /etc/nginx/conf.d/production.conf
+	sudo sed -i "s|/var/www/maintenance|/var/www/example|g" /etc/nginx/conf.d/maintenance.conf
         sudo $serv1 restart
         sudo $serv2 restart
 }
